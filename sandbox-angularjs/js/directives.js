53c747bfd6e20dca0b22c7c96a303b301be80d97
Kevin Chan <chessmasterhong@yahoo.com>
2014-06-06 09:44:36 -0400

    Added directive transclusion.

--- sandbox-angularjs/js/directives.js
+++ sandbox-angularjs/js/directives.js
@@ -3,12 +3,13 @@
 angular.module('demoApp.directives', [])
 .directive('simpleCheckbox', function() {
     var template = '\
-        <input type="checkbox">\
+        <input type="checkbox"> <span ng-transclude></span>\
     ';
 
     return {
         restrict: 'A',
         scope: { data: '=' },
+        transclude: true,
         template: template
     };
 })
